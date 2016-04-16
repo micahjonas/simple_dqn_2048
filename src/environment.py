70558bf4091af6b1d8dc11ae7c11cda1fd133560
Micha Schwendener <micha.schwendener@gmail.com>
2016-04-16 02:36:01 +0200

    changed the environment to 2048

--- src/environment.py
+++ src/environment.py
@@ -1,49 +1,22 @@
 import sys
 import os
-from ale_python_interface import ALEInterface
+from game import Game
 import cv2
 import logging
 logger = logging.getLogger(__name__)
 
 class Environment:
   def __init__(self, rom_file, args):
-    self.ale = ALEInterface()
+    self.game = Game()
     if args.display_screen:
-      if sys.platform == 'darwin':
-        import pygame
-        pygame.init()
-        self.ale.setBool('sound', False) # Sound doesn't work on OSX
-      elif sys.platform.startswith('linux'):
-        self.ale.setBool('sound', True)
-      self.ale.setBool('display_screen', True)
-
-    self.ale.setInt('frame_skip', args.frame_skip)
-    self.ale.setFloat('repeat_action_probability', args.repeat_action_probability)
-    self.ale.setBool('color_averaging', args.color_averaging)
+      # DO SOME VISUALISATION
 
     if args.random_seed:
       self.ale.setInt('random_seed', args.random_seed)
 
-    if args.record_screen_path:
-      if not os.path.exists(args.record_screen_path):
-        logger.info("Creating folder %s" % args.record_screen_path)
-        os.makedirs(args.record_screen_path)
-      logger.info("Recording screens to %s", args.record_screen_path)
-      self.ale.setString('record_screen_dir', args.record_screen_path)
+    self.actions = self.game.getActionSet()
+    logger.info("Using full action set with size %d" % len(self.actions))
 
-    if args.record_sound_filename:
-      logger.info("Recording sound to %s", args.record_sound_filename)
-      self.ale.setBool('sound', True)
-      self.ale.setString('record_sound_filename', args.record_sound_filename)
-
-    self.ale.loadROM(rom_file)
-
-    if args.minimal_action_set:
-      self.actions = self.ale.getMinimalActionSet()
-      logger.info("Using minimal action set with size %d" % len(self.actions))
-    else:
-      self.actions = self.ale.getLegalActionSet()
-      logger.info("Using full action set with size %d" % len(self.actions))
     logger.debug("Actions: " + str(self.actions))
 
     self.dims = (args.screen_height, args.screen_width)
@@ -52,16 +25,17 @@ class Environment:
     return len(self.actions)
 
   def restart(self):
-    self.ale.reset_game()
+    self.game.reset()
 
   def act(self, action):
-    reward = self.ale.act(self.actions[action])
+    reward = self.game.move(self.actions[action])
+
     return reward
 
   def getScreen(self):
-    screen = self.ale.getScreenGrayscale()
-    resized = cv2.resize(screen, self.dims)
-    return resized
+    for line in self.game.getCellsLog2()
+        line = line.map()
+    return
 
   def isTerminal(self):
-    return self.ale.game_over()
+    return not self.game.canMove()
---
+++ src/game.py
@@ -0,0 +1,206 @@
+# -*- coding: UTF-8 -*-
+import random
+import numpy as np
+
+# PY3 compat
+try:
+    xrange
+except NameError:
+    xrange = range
+
+
+class Game(object):
+    """
+    A 2048 board
+    """
+
+    UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
+
+    SIZE = 4
+
+    def __init__(self, size=SIZE, **kws):
+        self.__size = size
+        self.__size_range = xrange(0, self.__size)
+        self.cells = [[0]*self.__size for _ in xrange(self.__size)]
+        self.addTile()
+        self.addTile()
+        self.score = 0
+
+    def size(self):
+        """return the board size"""
+        return self.__size
+
+    def score(self):
+        """return the score"""
+        return self.__size
+
+    def canMove(self):
+        """
+        test if a move is possible
+        """
+        if not self.filled():
+            return True
+
+        for y in self.__size_range:
+            for x in self.__size_range:
+                c = self.getCell(x, y)
+                if (x < self.__size-1 and c == self.getCell(x+1, y)) \
+                   or (y < self.__size-1 and c == self.getCell(x, y+1)):
+                    return True
+
+        return False
+
+    def filled(self):
+        """
+        return true if the game is filled
+        """
+        return len(self.getEmptyCells()) == 0
+
+    def addTile(self, value=None, choices=([2]*9+[4])):
+        """
+        add a random tile in an empty cell
+          value: value of the tile to add.
+          choices: a list of possible choices for the value of the tile.
+                   default is [2, 2, 2, 2, 2, 2, 2, 2, 2, 4].
+        """
+        if value:
+            choices = [value]
+
+        v = random.choice(choices)
+        empty = self.getEmptyCells()
+        if empty:
+            x, y = random.choice(empty)
+            self.setCell(x, y, v)
+
+    def getCells(self):
+        """return all the cells"""
+        return self.cells
+
+    def __transform(self, number):
+        if number == 0:
+            return 0
+        else:
+            return int(np.log2(number))
+
+    def getCellsLog2(self):
+        return [[self.__transform(x) for x in line] for line in self.cells]
+
+    def getCell(self, x, y):
+        """return the cell value at x,y"""
+        return self.cells[y][x]
+
+    def setCell(self, x, y, v):
+        """set the cell value at x,y"""
+        self.cells[y][x] = v
+
+    def getLine(self, y):
+        """return the y-th line, starting at 0"""
+        return self.cells[y]
+
+    def getCol(self, x):
+        """return the x-th column, starting at 0"""
+        return [self.getCell(x, i) for i in self.__size_range]
+
+    def setLine(self, y, l):
+        """set the y-th line, starting at 0"""
+        self.cells[y] = l[:]
+
+    def setCol(self, x, l):
+        """set the x-th column, starting at 0"""
+        for i in xrange(0, self.__size):
+            self.setCell(x, i, l[i])
+
+    def getEmptyCells(self):
+        """return a (x, y) pair for each empty cell"""
+        return [(x, y)
+                for x in self.__size_range
+                for y in self.__size_range if self.getCell(x, y) == 0]
+
+    def __collapseLineOrCol(self, line, d):
+        """
+        Merge tiles in a line or column according to a direction and return a
+        tuple with the new line and the score for the move on this line
+        """
+        if (d == Game.LEFT or d == Game.UP):
+            inc = 1
+            rg = xrange(0, self.__size-1, inc)
+        else:
+            inc = -1
+            rg = xrange(self.__size-1, 0, inc)
+
+        pts = 0
+        for i in rg:
+            if line[i] == 0:
+                continue
+            if line[i] == line[i+inc]:
+                v = line[i]*2
+                line[i] = v
+                line[i+inc] = 0
+                pts += v
+
+        return (line, pts)
+
+    def __moveLineOrCol(self, line, d):
+        """
+        Move a line or column to a given direction (d)
+        """
+        nl = [c for c in line if c != 0]
+        if d == Game.UP or d == Game.LEFT:
+            return nl + [0] * (self.__size - len(nl))
+        return [0] * (self.__size - len(nl)) + nl
+
+    def getGrid(self):
+        """
+        Returns the grid
+        """
+        return sefl.cells
+
+    def getActionSet(self):
+        return [0,1,2,3]
+
+    def reset(self):
+        self.__size = Game.SIZE
+        self.__size_range = xrange(0, self.__size)
+        self.cells = [[0]*self.__size for _ in xrange(self.__size)]
+        self.addTile()
+        self.addTile()
+        self.score = 0
+
+    def move(self, d, add_tile=True):
+        """
+        move and return the grid
+        """
+        if d == Game.LEFT or d == Game.RIGHT:
+            chg, get = self.setLine, self.getLine
+        elif d == Game.UP or d == Game.DOWN:
+            chg, get = self.setCol, self.getCol
+        else:
+            return self.cells
+
+        moved = False
+        scoreOfRound = 0
+
+        for i in self.__size_range:
+            # save the original line/col
+            origin = get(i)
+            # move it
+            line = self.__moveLineOrCol(origin, d)
+            # merge adjacent tiles
+            collapsed, pts = self.__collapseLineOrCol(line, d)
+            # move it again (for when tiles are merged, because empty cells are
+            # inserted in the middle of the line/col)
+            new = self.__moveLineOrCol(collapsed, d)
+            # set it back in the board
+            chg(i, new)
+            # did it change?
+            if origin != new:
+                moved = True
+            scoreOfRound += pts
+
+        self.score += scoreOfRound
+
+        # don't add a new tile if nothing changed
+        if moved and add_tile:
+            self.addTile()
+
+        return scoreOfRound
